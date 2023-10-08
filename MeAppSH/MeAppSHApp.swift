7543dbaca6e7a2d424c7d612bf2b71125cf8ca65
Shua'a <51505005+Shuaa-99@users.noreply.github.com>
2023-10-08 11:16:33 +0300

    Initial Commit

---
+++ MeAppSH/MeAppSHApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeAppSHApp.swift
+//  MeAppSH
+//
+//  Created by SHUAA on 8.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeAppSHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
